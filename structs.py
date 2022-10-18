4dd240df6ede349c73d3bf06e0b466a5c8e75d49
Martin Wighus Holtmon <martinwh@stud.ntnu.no>
2022-10-18 19:48:27 +0200

    created the database document schemas

---
+++ structs.py
@@ -0,0 +1,37 @@
+"""The dataclasses that are insert into the mongodb"""
+from dataclasses import dataclass
+from datetime import datetime
+
+
+@dataclass
+class TrackPoint:
+    """A trackpoint in the database for an activity"""
+
+    # _id: int
+    activity_id: int  # Referencing parent
+    lat: float
+    lon: float
+    altitude: int
+    date_days: int
+    date_time: datetime
+
+
+@dataclass
+class Activity:
+    """An activity in the database for a user"""
+
+    # _id: int
+    user_id: str  # Referencing parent
+    transportation_mode: str
+    start_date_time: datetime
+    end_date_time: datetime
+    trackpoints: list  # list with _id of trackpoints
+
+
+@dataclass
+class User:
+    """A user in the database"""
+
+    _id: str
+    has_label: bool
+    # activities: dict  # List of activities with transportation_mode
